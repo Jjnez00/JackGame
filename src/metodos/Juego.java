99c6e2a5554fc3ad1bcc290e47ae503827d07783
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2022-07-07 01:44:05 +0200

    Update and reformated sheaty code

--- src/metodos/Juego.java
+++ src/metodos/Juego.java
@@ -13,105 +13,104 @@ import java.util.ArrayList;
  */
 public class Juego {
 
-    private  static int Puntos = 0;
-    private  static ArrayList<Player> Posisiones= new ArrayList<Player>();
-    private  static ArrayList<ListaDeProblemas> ProblemasDeTipoSeleccion=new ArrayList<ListaDeProblemas>();
+    private static Juego instancia = null;
+    
+    private int puntos = 0;
+    private int problemaActual = 0;
+    private ArrayList<Player> posisiones = new ArrayList<>();
+    private ArrayList<Problema> problemasDeTipoSeleccion = new ArrayList<>();
+    private ArrayList<Problema> ProblemasDeTipoPoner = new ArrayList<>();
+    private boolean juegoTerminado = false;
+    private int tipoJuego = 0;
 
-    public static int getPuntos() {
-        return Puntos;
+    public static Juego getInstancia() {
+        if (instancia == null) {
+            instancia = new Juego();
+        }
+        return instancia;
     }
 
-    public static void setPuntos(int Puntos) {
-        Juego.Puntos = Puntos;
+    public Problema nextProblema() {
+        ArrayList<Problema> problemas;
+        if (tipoJuego == 0) {
+            problemas = problemasDeTipoSeleccion;
+        } else {
+            problemas = ProblemasDeTipoPoner;
+        }
+        Problema p = problemas.get(problemaActual++);
+        if (problemaActual == problemas.size()) {
+            juegoTerminado = true;
+        }
+        return p;
     }
 
-    public static ArrayList<Player> getPosisiones() {
-        return Posisiones;
+    public int getPuntos() {
+        return puntos;
     }
 
-    public static void setPosisiones(ArrayList<Player> Posisiones) {
-        Juego.Posisiones = Posisiones;
+    public void setPuntos(int Puntos) {
+        this.puntos = Puntos;
     }
 
-    public static ArrayList<ListaDeProblemas> getProblemasDeTipoSeleccion() {
-        return ProblemasDeTipoSeleccion;
+    public ArrayList<Player> getPosisiones() {
+        return posisiones;
     }
 
-    public static void setProblemasDeTipoSeleccion(ArrayList<ListaDeProblemas> ProblemasDeTipoSeleccion) {
-        Juego.ProblemasDeTipoSeleccion = ProblemasDeTipoSeleccion;
+    public void setPosisiones(ArrayList<Player> Posisiones) {
+        this.posisiones = Posisiones;
     }
 
-    public static ArrayList<ListaDeProblemas> getProblemasDeTipoPoner() {
+    public ArrayList<Problema> getProblemasDeTipoSeleccion() {
+        return problemasDeTipoSeleccion;
+    }
+
+    public void setProblemasDeTipoSeleccion(ArrayList<Problema> ProblemasDeTipoSeleccion) {
+        this.problemasDeTipoSeleccion = ProblemasDeTipoSeleccion;
+    }
+
+    public ArrayList<Problema> getProblemasDeTipoPoner() {
         return ProblemasDeTipoPoner;
     }
 
-    public static void setProblemasDeTipoPoner(ArrayList<ListaDeProblemas> ProblemasDeTipoPoner) {
-        Juego.ProblemasDeTipoPoner = ProblemasDeTipoPoner;
+    public void setProblemasDeTipoPoner(ArrayList<Problema> ProblemasDeTipoPoner) {
+        this.ProblemasDeTipoPoner = ProblemasDeTipoPoner;
     }
-    private  static ArrayList<ListaDeProblemas> ProblemasDeTipoPoner=new ArrayList<ListaDeProblemas>();
 
-    private int[] sumandoSeleccion1 = new int[]{
-      4,10,7,15,20,8,79,63,38,7
-    };
-    private int[] sumandoSeleccion2 = new int[]{
-    3,5,32,3,13,6,15,7,16,6
-    };
-    private int[] respuestaSeleccion = new int[]{
-    1,50,39,5,7,48,94,9,22,42
-    };
-    private char[] operadorSeleccion = new char[]{
-     '-','*','+','/','-','*','+','/','-','*'
-    };
-    private int[] sumandoPoner1 = new int[]{
-      11,4,21,43,86,5,31,18,94,8
-    };
-    private int[] sumandoPoner2 = new int[]{
-    8,3,3,32,45,4,19,3,71,9
-    };
-    private int[] respuestaPoner = new int[]{
-    3,12,7,75,41,20,50,6,23,81
-    };
-    private char[] operadorPoner = new char[]{
-     '-','*','/','+','-','*','+','/','-','*'
-    };
-    public void DefaultProblemas() {
+    public void defaultProblemas() {
         for (int i = 0; i < 10; i++) {
-            ListaDeProblemas p = new ListaDeProblemas(sumandoSeleccion1[i], operadorSeleccion[i], sumandoSeleccion2[i], respuestaSeleccion[i]);
-            ProblemasDeTipoSeleccion.add(p);
-            
+            problemasDeTipoSeleccion.add(new Problema());
         }
         for (int i = 0; i < 10; i++) {
-            ListaDeProblemas p = new ListaDeProblemas(sumandoPoner1[i], operadorPoner[i], sumandoPoner2[i], respuestaPoner[i]);
-            ProblemasDeTipoPoner.add(p);
+            ProblemasDeTipoPoner.add(new Problema());
         }
     }
 
     public void AgregarProblemaDeSeleccion(int sumando1, char operandor, int sumando2, int respuesta) {
-        ListaDeProblemas p = new ListaDeProblemas(sumando1, operandor, sumando2, respuesta);
-        ProblemasDeTipoSeleccion.add(p);
+        Problema p = new Problema(sumando1, operandor, sumando2, respuesta);
+        problemasDeTipoSeleccion.add(p);
     }
 
     public void AgregarProblemaDePoner(int sumando1, char operandor, int sumando2, int respuesta) {
-        ListaDeProblemas p = new ListaDeProblemas(sumando1, operandor, sumando2, respuesta);
+        Problema p = new Problema(sumando1, operandor, sumando2, respuesta);
         ProblemasDeTipoPoner.add(p);
     }
-    
-    public void AgregarJugador(String nombre , int puntos){
+
+    public void AgregarJugador(String nombre, int puntos) {
         Player p = new Player(nombre, puntos);
-        Posisiones.add(p);
+        posisiones.add(p);
     }
 
     public void SistemaDePuntos(int n) {
         if (n == 1) {
-            Puntos += 5;
+            puntos += 5;
         }
         if (n == 0) {
-            Puntos += 2;
+            puntos += 2;
         }
     }
 
     public void RestPuntos() {
-        Puntos = 0;
+        puntos = 0;
     }
 
 }
